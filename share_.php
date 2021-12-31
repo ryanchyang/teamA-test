87b3e9a400e67ab6d57d788762058d581ec77eb6
Ryan Yang <ryanyang83@gmail.com>
2021-12-31 14:08:49 +0800

    history page

--- share_.php
+++ share_.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/parts/__connect_db.php';
 
-$sql = " SELECT `c_product_name`, `c_product_img_path`
+$co_sql = " SELECT `c_product_name`, `c_product_img_path`
 FROM `classic_product` cp
 JOIN `classic_orders` co 
 ON cp.`c_product_id` = co.`product_id`
@@ -9,7 +9,17 @@ JOIN `cart` c
 ON co.`member_id`= c.`cart_member_id`
 WHERE c.`cart_status`='3'    
 AND co.`member_id`=1 AND cp.`c_product_category` != 'bx' ";
-$rows = $pdo->query($sql)->fetchAll();
+$co_rows = $pdo->query($co_sql)->fetchAll();
+
+$cuso_sql = " SELECT `cm_product_name`
+FROM `customized_product` cusp
+JOIN `customized_orders` cuso
+ON cusp.`cm_product_id` = cuso.`customed_product_id`
+JOIN `cart` c 
+ON cuso.`member_id`= c.`cart_member_id`
+WHERE c.`cart_status`='3'    
+AND cuso.`member_id`=1 ";
+$cuso_rows = $pdo->query($cuso_sql)->fetchAll();
 
 
 ?>
@@ -21,28 +31,54 @@ $rows = $pdo->query($sql)->fetchAll();
   <div class="row justify-content-center">
     <div class="col-lg-6">
 
-      <table class="table table-striped table-bordered">
+    <div>
+      <h2 class="fw-bold py-5">分享產品</h2>
+    </div>
+      <table class="table table-striped mb-5">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">會員ID</th>
-           
+            <th scope="col">經典商品</th>
+            <th scope="col"></th>
+            <th scope="col"></th>          
           </tr>
         </thead>
         <tbody>
         
-          <?php foreach($rows as $row): ?>
+          <?php foreach($co_rows as $row): ?>
           
-          <tr>
-            <th scope="row"><?= $row['c_product_name'];?></th>
-            <th scope="row"><img src=" <?= $row['c_product_img_path'];?>" alt="" style="width:100px;"> </th>       
-            <th scope="row"><?= $row['share_status'] = '1' ? "已分享" : "未分享";?></th>
+          <tr class=" ">
+            <th scope="row" class="py-5 ps-3"><?= $row['c_product_name'];?></th>
+            <th scope="row"><img src=" <?= $row['c_product_img_path'];?>" alt="" style="width:100px;"></th>       
+            <th scope="row" class="pt-5  text-end"><button  button type="button" class="btn btn-primary ">分享</button><button type="button" class="btn btn-secondary ms-2">刪除</button></th>       
+            <!-- <th scope="row"><?= $row['share_status'] = '1' ? "已分享" : "未分享";?></th> -->
           </tr>
           <?php endforeach; ?>
         </tbody>
 
       </table>
-    </div>
+
+      <table class="table table-striped ">
+        <thead>
+          <tr>
+            <th scope="col">客製化商品</th>
+            <th scope="col"></th>
+            <th scope="col"></th>          
+          </tr>
+        </thead>
+        <tbody>
+        
+          <?php foreach($cuso_rows as $row): ?>
+          
+          <tr class=" ">
+            <th scope="row" class="py-5 ps-3"><?= $row['cm_product_name'];?></th>
+            <th scope="row" class="pt-5  text-end"><button  button type="button" class="btn btn-primary ">分享</button><button type="button" class="btn btn-secondary ms-2">刪除</button></th>       
+            <!-- <th scope="row"><?= $row['share_status'] = '1' ? "已分享" : "未分享";?></th> -->
+          </tr>
+          <?php endforeach; ?>
+        </tbody>
+
+      </table>
+    </div>  
   </div>
 </div>
 
