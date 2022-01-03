b80f727f721108eb2133356d93b7a4fe38c11294
Ryan Yang <ryanyang83@gmail.com>
2022-01-03 21:28:11 +0800

    share preview

--- share_preview.php
+++ share_preview.php
@@ -1,15 +1,15 @@
 <?php
 require __DIR__ . '/parts/__connect_db.php';
 
-$co_sql = "SELECT `share_title`, `share_img`
+$co_sql = "SELECT *
 FROM `share_item` s
-WHERE s.`share_order_category`='co' AND s.`share_status` = 0
+WHERE s.`share_order_category`='co' AND s.`share_status` = 1
 AND s.`member_id`=1";
 $co_rows = $pdo->query($co_sql)->fetchAll();
 
-$cuso_sql = "SELECT `share_title`, `share_img`
+$cuso_sql = "SELECT *
 FROM `share_item` s
-WHERE s.`share_order_category`='cuso' AND s.`share_status` = 0  
+WHERE s.`share_order_category`='cuso' AND s.`share_status` = 1  
 AND s.`member_id`=1";
 $cuso_rows = $pdo->query($cuso_sql)->fetchAll();
 
@@ -17,61 +17,43 @@ $cuso_rows = $pdo->query($cuso_sql)->fetchAll();
 ?>
 <?php include __DIR__ . '/parts/__html_head.php' ?>
 <?php include __DIR__ . '/parts/__navbar.php' ?>
-
+<style>
+  .tag {
+    background: #eee;
+    padding: 5px 10px;
+    text-decoration: none ;
+    border-radius: 10px;
+    margin-right: 5px;
+  }
+</style>
 
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-lg-6">
 
     <div>
-      <a href="#">待分享區</a>
+      <a href="share.php">待分享區</a>
       <a href="share_shared.php">已分享區</a>
-      <a href="share_preview.php">預覽牆</a>
+      <a href="#">預覽牆</a>
     </div>
-      <table class="table table-striped mb-5">
-        <thead>
-          <tr>
-            <th scope="col">經典商品</th>
-            <th scope="col"></th>
-            <th scope="col"></th>          
-          </tr>
-        </thead>
-        <tbody>
-        
-          <?php foreach($co_rows as $row): ?>
-          
-          <tr class=" ">
-            <th scope="row" class="py-5 ps-3"><?= $row['share_title'];?></th>
-            <th scope="row"><img src=" <?= $row['share_img'];?>" alt="" style="width:100px;"></th>       
-            <th scope="row" class="pt-5  text-end"><button  button type="button" class="btn btn-primary ">分享</button><button type="button" class="btn btn-secondary ms-2">刪除</button></th>       
-            <!-- <th scope="row"><?= $row['share_status'] = '1' ? "已分享" : "未分享";?></th> -->
-          </tr>
-          <?php endforeach; ?>
-        </tbody>
-
-      </table>
-
-      <table class="table table-striped ">
-        <thead>
-          <tr>
-            <th scope="col">客製化商品</th>
-            <th scope="col"></th>
-            <th scope="col"></th>          
-          </tr>
-        </thead>
-        <tbody>
-        
-          <?php foreach($cuso_rows as $row): ?>
-          
-          <tr class=" ">
-            <th scope="row" class="py-5 ps-3"><?= $row['share_title'];?></th>
-            <th scope="row" class="pt-5  text-end"><button  button type="button" class="btn btn-primary ">分享</button><button type="button" class="btn btn-secondary ms-2">刪除</button></th>       
-            <!-- <th scope="row"><?= $row['share_status'] = '1' ? "已分享" : "未分享";?></th> -->
-          </tr>
-          <?php endforeach; ?>
-        </tbody>
-
-      </table>
+   
+   <div class="row d-flex flex-row justify-content-between">
+    <?php foreach($co_rows as $row): ?>
+   
+    <div class="card" style="width: 20rem;">
+  <img src="<?= $row['share_img'];?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?= $row['share_title'];?></h5>
+    <p class="card-text"><?= $row['share_desc'];?></p><br>
+    <p class="card-text"><small class="text-muted">Last updated <?= $row['share_date'];?></small></p>
+    <p class="card-text"><small class="text-muted"><i class="far fa-thumbs-up"> <?= $row['share_liked'];?></i></small></p> 
+    <a href="#" class="tag <?=empty(explode("," ,$row['share_hash'])[0]) ? 'visually-hidden' : ''?>"><small class="text-muted"> <?=explode("," ,$row['share_hash'])[0] ?? ''?></small></a>  
+    <a href="#" class="tag <?=empty(explode("," ,$row['share_hash'])[1]) ? 'visually-hidden' : ''?>"><small class="text-muted "> <?=explode("," ,$row['share_hash'])[1] ?? ''?></small></a>  
+    <a href="#" class="tag <?=empty(explode("," ,$row['share_hash'])[2]) ? 'visually-hidden' : ''?>"><small class="text-muted "> <?=explode("," ,$row['share_hash'])[2] ?? ''?></small></a>  
+  </div>
+</div>
+<?php endforeach; ?>
+</div>
     </div>  
   </div>
 </div>
